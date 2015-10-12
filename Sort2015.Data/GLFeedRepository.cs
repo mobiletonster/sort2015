8f25965661e3d7b06900f2927d603075a518d0fc
mobiletonster <mobiletony@msn.com>
2015-10-12 10:53:38 -0600

    updated starting point with backing services and repo methods.

--- Sort2015.Data/GLFeedRepository.cs
+++ Sort2015.Data/GLFeedRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Sort2015.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,8 @@ namespace Sort2015.Data
     public interface IGLFeedRepository
     {
         IQueryable<DailyGem> GetDailyGems();
+        object AddRecord(object entity);
+        object UpdateRecord(object entity);
     }
     public class GLFeedRepository: IGLFeedRepository
     {
@@ -28,5 +32,73 @@ namespace Sort2015.Data
         {
             return _context.DailyGems;
         }
+
+        // All purpose Add Record method
+        public object AddRecord(object entity)
+        {
+            var dbEntity = _context.Entry(entity);
+            dbEntity.State = EntityState.Added;
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                string errMessage = string.Empty;
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validErr in validationErrors.ValidationErrors)
+                    {
+                        errMessage += string.Format("Property: {0} Error:{1}", validErr.PropertyName, validErr.ErrorMessage);
+                    }
+
+                }
+                throw new Exception(errMessage);
+            }
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                dbEntity.State = EntityState.Detached;
+            }
+            return dbEntity.Entity;
+        }
+
+        // All purpose Update Record method
+        public object UpdateRecord(object entity)
+        {
+            var dbEntity = _context.Entry(entity);
+            dbEntity.State = EntityState.Modified;
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                string errMessage = string.Empty;
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validErr in validationErrors.ValidationErrors)
+                    {
+                        errMessage += string.Format("Property: {0} Error:{1}", validErr.PropertyName, validErr.ErrorMessage);
+                    }
+
+                }
+                throw new Exception(errMessage);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                dbEntity.State = EntityState.Detached;
+            }
+            return dbEntity.Entity;
+        }
+
     }
 }
